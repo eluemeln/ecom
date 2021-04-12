ed7a500371ee8b64319cd04189b3726c55c20997
eluemeln <47062807+eluemeln@users.noreply.github.com>
2021-04-12 09:26:58 -0600

    Update cart.php

--- public/cart.php
+++ public/cart.php
@@ -2,8 +2,6 @@
 
 
     
-
-
 <?php  
 
 
@@ -39,8 +37,12 @@ if(isset($_GET['remove'])){
     $_SESSION['product_'.$_GET['remove']]--;
     
     if($_SESSION['product_'.$_GET['remove']]<1)
+        
     {
-        redirect("checkout.php");
+            unset($_SESSION['item_total']);
+            unset($_SESSION['item_quantity']);
+        
+            redirect("checkout.php");
         
     }
     
@@ -55,6 +57,9 @@ if(isset($_GET['delete']))
 {
     
     $_SESSION['product_'.$_GET['delete']]= '';
+    unset($_SESSION['item_total']);
+    unset($_SESSION['item_quantity']);
+    
     redirect("checkout.php");
     
     
@@ -63,7 +68,12 @@ if(isset($_GET['delete']))
 function cart() 
    
 {
-         $total=0;
+        $total=0;
+        $item_quantity= 0;
+        $item_name=1;
+        $item_number=1;
+        $amount =1;
+        $quantity=1;
   
        foreach($_SESSION as $name =>$value){
            
@@ -77,11 +87,12 @@ function cart()
                 $id = substr($name,8,$len);
                     
 
-            $query = query("SELECT * FROM products WHERE product_id= " . escape_string($id). " ");
+            $query = query("SELECT * FROM products WHERE product_id= ".escape_string($id). " ");
             confirm($query);
             while($row=mysqli_fetch_array($query))
             {
                 $sub = $row['product_price'] * $value;
+                $item_quantity += $value;
 
                 $product =  <<<DELIMETER
                     <tr>
@@ -100,45 +111,60 @@ function cart()
 
 
 
-
-                            <a class='btn btn-danger' href="cart.php?delete={$row['product_id']}"><span class='glyphicon glyphicon-remove'</a>
-
-                        </td> 
+                            <a class='btn btn-danger' href="cart.php?delete={$row['product_id']}"><span class='glyphicon glyphicon-remove'</a></td> 
 
                     </tr>
+                    
+                    <input type="hidden" name= "item_name_{$item_name}"  value="{$row['product_title']}">
+                    
+                    <input type="hidden" name= "item_number_{$item_number}"  value="{$row['product_id']}">
+                    
+                    <input type="hidden" name= "amount_{$amount}"  value="{$row['product_price']}">
+                    
+                    <input type="hidden" name= "quantity_{$quantity}"  value="{$value}">
                     
 
                     DELIMETER;
                     echo $product;
+               
 
-                    
 
                 }
-                $total= 0;
-                    
+                 $item_name++;
+                $item_number++;
+                $amount++;
+                $quantity++;
                 
-                //echo $total;
-                $_SESSION['item_total'] = $total+= $sub;
                 
-            
+                $total += $sub;
+                $_SESSION['item_total']= $total;
+                $_SESSION['item_quantity']= $item_quantity;
                 
-                 
-            
             
             }
            }
     
         
     }
+     
     
-        
-        
-        
-        
-        
-        
-        
+}
+function show_paypal(){
+    $paypal_button='';
     
+    if(isset($_SESSION['item_quantity'])&& isset($_SESSION['item_quantity'])>=1)
+    {
+    
+        $paypal_button= <<<DELIMETER
+
+        <input type="image" name="upload" border= "0" height="45" width="150"
+            src="https://www.paypalobjects.com/en_US/i/btn/btn_paynow_LG.gif"
+            alt="PayPal - The safer, easier way to pay online">
+
+
+        DELIMETER;
+         return $paypal_button;
+    }
     
     
     
@@ -146,5 +172,7 @@ function cart()
 
 
 
-
 ?>
+    
+
+
